135f57ac9d68861ce8d35c946ca83245ff3d9be3
Otavio Monteiro <otavvioo11@gmail.com>
2023-11-24 11:16:50 -0300

    Atualizacao

--- app/src/main/java/com/example/sorvetinhos/MainActivity.java
+++ app/src/main/java/com/example/sorvetinhos/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.sorvetinhos;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,
+                        Tela02.class);
+                startActivity(intent);
+            }
+        },2000);
     }
 }
